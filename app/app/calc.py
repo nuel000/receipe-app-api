9f51639b942841fea335537ccd387d20c4dfcd30
Emmanuel Momoh <momohemmanuel370@gmail.com>
2024-10-28 16:24:51 +0100

    configured DB

---
+++ app/app/calc.py
@@ -0,0 +1,17 @@
+"""
+
+Calculator functions
+
+"""
+
+def add(x,y):
+    """
+    Adds 2 numbers together
+    """
+    return x+y
+
+def subtract(x,y):
+    """
+    Subtracts one number from the other
+    """
+    return x-y
